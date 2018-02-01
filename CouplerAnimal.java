0cabfb097aa187cd9bab947d046f1e80c36bcbc8
Zekel97 <szandreas97@gmail.com>
2018-02-01 16:03:11 +0100

    Small add

--- CouplerAnimal.java
+++ CouplerAnimal.java
@@ -2,7 +2,32 @@
 public class CouplerAnimal {
 
 	public CouplerAnimal() {
-		// TODO Auto-generated constructor stub
+		
+		private int life=10;
+		private int posX;
+		private int posY;
+		
+		public CouplerAnimal(int x,int y)
+		{
+			life=10;
+			setPos(x,y);
+		}
+		
+		private void setPos(int x,int y)
+		{
+			posX=x;
+			posY=y;
+		}
+		
+		public int getPosX()
+		{
+			return posX;
+		}
+		
+		public int getPosY()
+		{
+			return posY;
+		}
 	}
 
 }
--- RunTime.java
+++ RunTime.java
@@ -3,13 +3,33 @@ import java.util.*;
 public class RunTime {
 
 	public static void main(String[] args) {
-	 
-    int h = 4;
-    int w = 4;
-    @SuppressWarnings("unused")
-	Environment game = new Environment(h, w);
-     
-     
-	}
+	
+		int h = 4;
+		int w = 4;
+		
+    /*
+		@SuppressWarnings("unused")
+		Environment game = new Environment(h, w);
+	*/     
+	    
+	    private boolean isRunning;
+		 
+		public void gameLoop() throws InterruptedException
+		{
+			@SuppressWarnings("unused")
+			Environment game = new Environment(h, w);
+			
+			//spawn animali e cibo
+			
+		    while(isRunning)
+		    {
+		    	//doGameUpdates();
+		    	//updateInfo();
+		    	Thread.sleep(1000);
+		    }
+		    
+		}
+	     
+		}
 
 }
